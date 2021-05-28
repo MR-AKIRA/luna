7283fff3a9ceed5434986861c7f45247f5da012a
MR-DARKHACKER <84956751+MR-DARKHACKER@users.noreply.github.com>
2021-05-28 15:29:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,3 +13,8 @@ API_HASH = getenv("API_HASH")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+
+STREAM_URL=os.environ.get("STREAM_URL", "https://radioindia.net/radio/hungamanow/icecast.audio")
+ADMIN = os.environ.get("ADMINS", '')
+ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
+    
